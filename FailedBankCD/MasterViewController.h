a0cf3a3c2fc83cde57a1793aaf0f53d5f8d368d1
Michael <liukaiyi54@gmail.com>
2015-12-05 18:05:32 +0800

    first commit

---
+++ FailedBankCD/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  FailedBankCD
+//
+//  Created by Michael on 15/12/3.
+//  Copyright © 2015年 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ FailedBankCD/MasterViewController.m
@@ -0,0 +1,36 @@
+//
+//  MasterViewController.m
+//  FailedBankCD
+//
+//  Created by Michael on 15/12/3.
+//  Copyright © 2015年 Michael. All rights reserved.
+//
+
+#import "MasterViewController.h"
+
+@interface MasterViewController ()
+
+@end
+
+@implementation MasterViewController
+@synthesize managedObjectContext;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+
+@end
